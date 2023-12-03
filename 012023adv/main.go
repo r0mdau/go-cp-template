89cf6b63d9c91463c13fce2ec6851ccf16a2c676
Romain Dauby <romain.dauby@gmail.com>
2023-12-03 17:23:08 -0500

    Unused map

--- 012023adv/main.go
+++ 012023adv/main.go
@@ -17,18 +17,6 @@ func print(a ...interface{}) {
 	fmt.Println(a...)
 }
 
-// var strRune = map[string]rune{
-// 	"one":   '1',
-// 	"two":   '2',
-// 	"three": '3',
-// 	"four":  '4',
-// 	"five":  '5',
-// 	"six":   '6',
-// 	"seven": '7',
-// 	"eight": '8',
-// 	"nine":  '9',
-// }
-
 var nMap = map[string]int{
 	"one":   1,
 	"two":   2,
